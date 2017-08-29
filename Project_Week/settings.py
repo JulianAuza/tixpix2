9278b1be65852ed36c386d70a1a04a798e55f008
JulianAuza <julianauza@gmail.com>
2017-08-29 14:11:56 -0500

    poop

--- Project_Week/settings.py
+++ Project_Week/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
     }
 }
 
-DATABASES['default'].update(db_from_env)
+# DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
