bfec315c27ab3f1aa7e7cb20b8f4022f9a69c0f8
JulianAuza <julianauza@gmail.com>
2017-08-30 14:02:59 -0500

    added all req uncommented db in settings

--- Project_Week/settings.py
+++ Project_Week/settings.py
@@ -82,9 +82,9 @@ DATABASES = {
     }
 }
 
-# import dj_database_url
-# db_from_env = dj_database_url.config()
-# DATABASES['default'].update(db_from_env)
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 # DATABASES['default']['CONN_MAX_AGE'] = 500
 
 
